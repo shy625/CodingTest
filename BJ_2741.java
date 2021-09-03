f7fb3ae32e3012f242bf584adf8acf6e3104ff25
shy625 <thgusdud123@gmail.com>
2021-09-04 05:10:27 +0900

    Add BJ 2741

---
+++ BJ_2741.java
@@ -0,0 +1,18 @@
+/**
+ * @ BJ 2741 N 찍기
+ * @ prob : https://www.acmicpc.net/problem/2741
+ */
+
+import java.util.Scanner;
+
+public class BJ_2741 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
