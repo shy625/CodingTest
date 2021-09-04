603fe4d78922572a09632b28df1a53f019add5ed
shy625 <thgusdud123@gmail.com>
2021-09-04 19:43:55 +0900

    Add BJ 2440 별 찍기 - 3

---
+++ BJ_2440.java
@@ -0,0 +1,19 @@
+/**
+ * @ BJ 2440 별 찍기 - 3
+ * @ prob : https://www.acmicpc.net/problem/2440
+ */
+
+import java.util.Scanner;
+
+public class BJ_2440 {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+
+        for(int i = n; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
