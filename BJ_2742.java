f6ba82a17027ba4fcf009ca93de0e502d562d568
shy625 <thgusdud123@gmail.com>
2021-09-04 18:22:34 +0900

    Add BJ 2742

---
+++ BJ_2742.java
@@ -0,0 +1,16 @@
+/**
+ * @ BJ 2742 기찍 N
+ * @ prob : https://www.acmicpc.net/problem/2742
+ */
+
+import java.util.Scanner;
+
+public class BJ_2742 {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+
+        for(int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
