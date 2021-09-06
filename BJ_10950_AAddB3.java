782e43b2cb4cfcbf9cc201176f1d37ef95bf5693
shy625 <thgusdud123@gmail.com>
2021-09-07 02:58:00 +0900

    Add BJ 10950 A+B - 3

---
+++ BJ_10950_AAddB3.java
@@ -0,0 +1,19 @@
+/**
+ * @ BJ 10950 A+B - 3
+ * @ prob : https://www.acmicpc.net/problem/10950
+ */
+
+import java.util.Scanner;
+
+public class BJ_10950_AAddB3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for(int i = 0; i < t; i++) {
+            System.out.println(sc.nextInt() + sc.nextInt());
+        }
+
+        sc.close();
+    }
+}
