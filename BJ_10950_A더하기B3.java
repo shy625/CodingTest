85c764a19e1c904c46ebc4bba9e56c384e4691e0
shy625 <thgusdud123@gmail.com>
2021-09-08 01:33:52 +0900

    Rename BJ_10950_A더하기B3.java BJ_10952_A더하기B5.java

--- BJ_10950_A더하기B3.java
+++ BJ_10950_A더하기B3.java
@@ -5,7 +5,7 @@
 
 import java.util.Scanner;
 
-public class BJ_10950_AAddB3 {
+public class BJ_10950_A더하기B3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- BJ_10952_A더하기B5.java
+++ BJ_10952_A더하기B5.java
@@ -5,7 +5,7 @@
 
 import java.util.Scanner;
 
-public class BJ_10952_AAddB5 {
+public class BJ_10952_A더하기B5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
