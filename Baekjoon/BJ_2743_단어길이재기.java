14b336bd469cc9b49fbed89aef8ee496932d7f9e
shy625 <thgusdud123@gmail.com>
2021-09-28 23:34:12 +0900

    Add BJ 2743 단어 길이 재기

---
+++ Baekjoon/BJ_2743_단어길이재기.java
@@ -0,0 +1,17 @@
+/**
+ * @ BJ 2743 단어 길이 재기
+ * @ prob : https://www.acmicpc.net/problem/2743
+ */
+
+import java.util.Scanner;
+
+public class BJ_2743_단어길이재기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String inputString = sc.nextLine();
+        System.out.println(inputString.length());
+
+        sc.close();
+    }
+}
