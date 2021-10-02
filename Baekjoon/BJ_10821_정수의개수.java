4580dc0b6ca618cb6f44eac2030f6027965eb58d
shy625 <thgusdud123@gmail.com>
2021-10-02 18:15:06 +0900

    Add BJ 10821 정수의 개수

---
+++ Baekjoon/BJ_10821_정수의개수.java
@@ -0,0 +1,19 @@
+/**
+ * @ BJ 10821 정수의 개수
+ * @ prob : https://www.acmicpc.net/problem/10821
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_10821_정수의개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(",");
+        System.out.println(input.length);
+
+        br.close();
+    }
+}
