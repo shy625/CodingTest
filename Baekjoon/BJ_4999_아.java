3277061bb2994111f8bd6bed954b09ca1fc19e18
shy625 <thgusdud123@gmail.com>
2021-10-11 23:33:27 +0900

    Add BJ 4999 아

---
+++ Baekjoon/BJ_4999_아.java
@@ -0,0 +1,20 @@
+/**
+ * @ BJ 4999 아
+ * @ prob : https://www.acmicpc.net/problem/4999
+ */
+
+import java.util.Scanner;
+
+public class BJ_4999_아 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        if(sc.nextLine().length() >= sc.nextLine().length()) {
+            System.out.println("go");
+        } else {
+            System.out.println("no");
+        }
+
+        sc.close();
+    }
+}
