92c5fafaef5ef2cca7eb865db44e3512be81a3d5
shy625 <thgusdud123@gmail.com>
2021-10-14 03:33:21 +0900

    Add BJ 10926 ??!

---
+++ Baekjoon/BJ_10926_QQE.java
@@ -0,0 +1,19 @@
+/**
+ * @ BJ 10926 ??!
+ * @ prob : https://www.acmicpc.net/problem/10926
+ */
+
+import java.io.*;
+
+public class BJ_10926_QQE {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine() + "??!");
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
