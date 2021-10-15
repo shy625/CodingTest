555a415a0c097914d036155d1867d86fe5ade158
shy625 <thgusdud123@gmail.com>
2021-10-15 09:02:12 +0900

    Add BJ 2420 사파리월드

---
+++ Baekjoon/BJ_2420_사파리월드.java
@@ -0,0 +1,16 @@
+/**
+ * @ BJ 2420 사파리월드
+ * @ prob : https://www.acmicpc.net/problem/2420
+ */
+
+import java.util.Scanner;
+
+public class BJ_2420_사파리월드 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(Math.abs(sc.nextLong() - sc.nextLong()));
+
+        sc.close();
+    }
+}
