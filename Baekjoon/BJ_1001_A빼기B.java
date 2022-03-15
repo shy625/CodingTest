887e8c3290485ca19ad03056a66e0b2497282e65
shy625 <thgusdud123@gmail.com>
2022-03-16 08:17:12 +0900

    Add BJ 1001 A-B

---
+++ Baekjoon/BJ_1001_A빼기B.java
@@ -0,0 +1,18 @@
+/**
+ * BJ 1001 A-B
+ * @ prob : https://www.acmicpc.net/problem/1001
+ * @ level : B5
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_1001_A빼기B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken()));
+    }   
+}
