de0fa7f152a65036ce43cbb44471fb092ecaaed8
shy625 <thgusdud123@gmail.com>
2022-03-17 08:46:25 +0900

    Add BJ 2557 Hello World

---
+++ Baekjoon/BJ_2557_HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * BJ 2557 Hello World
+ * @ prob : https://www.acmicpc.net/problem/2557
+ * @ level : B5
+ */
+
+public class BJ_2557_HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
