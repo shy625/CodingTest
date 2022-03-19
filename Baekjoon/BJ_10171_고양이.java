b8ef2d07d9cfc18df25e16b0065d92b7dbec4fc9
shy625 <thgusdud123@gmail.com>
2022-03-19 11:40:36 +0900

    Add BJ 10171 고양이

---
+++ Baekjoon/BJ_10171_고양이.java
@@ -0,0 +1,14 @@
+/**
+ * BJ 10171 고양이
+ * @ prob : https://www.acmicpc.net/problem/10171
+ * @ level : B5
+ */
+
+public class BJ_10171_고양이 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
