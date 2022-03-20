6df83c2cbfdd0cbcbff0e29b4a4afd5144061ac6
shy625 <thgusdud123@gmail.com>
2022-03-20 16:36:10 +0900

    Add BJ 10172 개

---
+++ Baekjoon/BJ_10172_개.java
@@ -0,0 +1,15 @@
+/**
+ * BJ 10172 개
+ * @ prob : https://www.acmicpc.net/problem/10172
+ * @ level : B5
+ */
+
+public class BJ_10172_개 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
